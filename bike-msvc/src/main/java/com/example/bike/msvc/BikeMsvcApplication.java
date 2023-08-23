7ebddcde47bf71ec38a0547ea3cc1007aed3cf91
Herbert-Loayza <loayza.herbert@gmail.com>
2023-08-22 21:45:48 -0500

    commit eureka

--- bike-msvc/src/main/java/com/example/bike/msvc/BikeMsvcApplication.java
+++ bike-msvc/src/main/java/com/example/bike/msvc/BikeMsvcApplication.java
@@ -3,6 +3,7 @@ package com.example.bike.msvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BikeMsvcApplication {
 
